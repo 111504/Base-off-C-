2366eb31a54cd322858d40d9a5cc3d98c9e50328
111504 <35070932+111504@users.noreply.github.com>
2020-02-01 19:16:37 +0800

    upload struct.c
    upload struct.c

---
+++ struct.c
@@ -0,0 +1,22 @@
+/*
+ * struct.c
+ *
+ *  Created on: 2020¦~2¤ë1¤é
+ *      Author: fish2
+ */
+
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+struct student{
+		char name[10];
+		int number;
+        char phone[10];
+        float grade[10];
+		int birthday,birthMonth;
+};
+
+
+
+
