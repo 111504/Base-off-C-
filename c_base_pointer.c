266b037781ecfb39795b2534809022d84f51125b
111504 <35070932+111504@users.noreply.github.com>
2020-02-18 19:31:55 +0800

    upload
    file  write c pointer

---
+++ c_base_pointer.c
@@ -0,0 +1,84 @@
+/*
+ ============================================================================
+ Name        : c_base.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+/*	puts("!!!Hello World!!!"); prints !!!Hello World!!!
+	return EXIT_SUCCESS;
+	\struct student jone={"John Smith",123,"0905015016",60,12,6};//宣告
+	struct student *ptr=&jone;
+
+	/*printf("name is %s\n",jone.name);
+	strcpy(jone.name,"John Amy");//指定欄位改值
+	printf("name is %s\n",jone.name);
+	printf("%ld \n",offsetof(struct student,number));
+	ptr->number=11345;//指標方式改值
+	printf("number is %ld\n",jone.number);*/
+  /*  printf("%d",12);*/ 
+	//////////////////////////////////////////
+	/*把結構當參數傳遞*/
+	char  *card="jqk";//不能更改 
+	char  card2[]="jdk";//能更改 
+	char *carrd=card;
+	
+	
+	printf("%s\n",card);
+	printf("%s\n",carrd);
+	/*char *a="123456";
+	char b[]="12345";
+	char *c=b;*/
+/*	int a1,a2;
+	scanf("%d %i",&a1,&a2);
+	printf("%d %i\n",a1,a2);
+	printf("%d %d\n",a1,a2);*/
+  //char a[15];
+  //char b[14]="HELLOWdas";
+  //char a[5];
+ // scanf("%4s",a);
+ // printf("%s\n",a);
+ // printf("%s\n",b);
+ // char *c=b;
+ // char *d=a;
+/*  printf("%s\n",c+2);
+  printf("%c\n",*c);
+  printf("%c\n",*(c+2));
+  printf("%p\n",&c);
+  printf("%d\n",sizeof(c));*/
+ // printf("%s\n",d);
+  
+  /*
+  b[2]='k';
+  printf("%p\n",b); 
+  printf("%p\n",c); 
+  c="aaa";
+  printf("%p\n",c); */
+  //int c[6]={1,2,3,4,5};
+ // scanf("%s\n",a);
+ // printf("%s\n",a); 
+  /*printf("%s\n",b); 
+  
+  printf("%s\n",c); 
+  printf("%s\n",b); */
+	//int a[6]={1,5,3,4,5,6};
+	//int *ptr=&a;
+//	puts()
+/**	printf("%p\n",b);
+	printf("%p\n",c);
+	printf("%s\n",c);*/
+	//print int array use loop 
+/*	printf("%p\n",a);//address of array
+   printf("%d\n",*a);//==a[0]
+	printf("%d\n",*a+1);
+	printf("%d\n",*(a+1));*/
+		
+}
+
